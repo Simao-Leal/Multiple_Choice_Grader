0ae4339b7c58c30eef5f302a0acdc4c936f1cc8a
Simao-Leal <simao.frg.leal@gmail.com>
2024-01-02 12:14:08 +0000

    sender first version

---
+++ src/sender/sender.py
@@ -0,0 +1,61 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import glob
+import json
+from pathlib import Path
+import getpass
+
+def send_email(sender_email, sender_password, recipient_email, subject, body, attachment_file_path):
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = recipient_email
+    message['Subject'] = subject
+    message.attach(MIMEText(body+'\n', 'plain'))
+
+    attachment = open(attachment_file_path, "rb")
+    filename = Path(attachment_file_path).name
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename="{filename}"')
+
+    message.attach(part)
+
+    # Connect to the SMTP server using SSL
+    with smtplib.SMTP_SSL('mail.tecnico.ulisboa.pt', 465) as server:
+        # Log in to the email account
+        server.login(sender_email, sender_password)
+
+        # Send the email
+        server.sendmail(sender_email, recipient_email, message.as_string())
+
+
+def sender():
+    sender_email = input('Please introduce your email: ')
+    sender_password = getpass.getpass(prompt='Please introduce your password: ')
+
+    #getting email list
+    files = glob.glob('output/evaluation_reports/*.json')
+    with open(files[0], 'r') as f:
+        email_adresses = json.load(f)
+    
+    with open('src/sender/email_text.txt', 'r') as f:
+        lines = f.readlines()
+        subject = lines[0]
+        body = '\n'.join(lines[1:])
+
+    evaluation_reports = glob.glob('output/evaluation_reports/*.pdf')
+    for path in evaluation_reports:
+        filename = Path(path).stem
+        print(email_adresses[filename])
+        receiver = 'simao.frg.leal@gmail.com' #email_adresses[filename]
+        send_email(sender_email, sender_password, receiver, subject, body, path)
+
+
+if __name__ == '__main__':
+    #send_email(sender_email, sender_password, recipient_email, subject, body, file_path)
+    sender()
